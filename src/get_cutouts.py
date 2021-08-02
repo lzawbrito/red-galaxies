05defdb3ce9bb3729c05a12cddddfe86d0e0d8fa
Lucas Brito <luzabri7@gmail.com>
2021-08-01 22:10:07 -0400

    U

--- src/get_cutouts.py
+++ src/get_cutouts.py
@@ -3,7 +3,27 @@ import pandas as pd
 from astropy.io import fits 
 
 IMAGE_CSV = 'files/cluster_images.csv'
+RED_GALAXIES_CSV = 'files/all_red_galaxies.csv'
 
 images_df = pd.read_csv(IMAGE_CSV)
-unique_clusters = images_df['cluster'].unique()
+
+# take head for now to avoid memory issues
+all_red_galaxies_df = pd.read_csv(RED_GALAXIES_CSV).head(n=100) 
+
+# obtain ra, dec dataframe 
+all_red_galaxies_coord_df = all_red_galaxies_df[['ra', 'dec']]
+
+# get one row per unique 
+unique_clusters_df = images_df.drop_duplicates(subset=['cluster'])
+
+for _, coord in all_red_galaxies_coord_df.iterrows():
+    for __, cluster in unique_clusters_df.iterrows():
+        data, header = fits.getdata(cluster['path'])
+        cg = CutoutGenerator(data, header)
+        if not cg.is_coord_in_image(coord['ra'], coord['dec']):
+            continue
+        
+        print('Coords ' + str(coord['ra']) + ', ' + str(coord['dec']) + \
+              ' found in cluster ' + str(cluster['path']))
+
 
