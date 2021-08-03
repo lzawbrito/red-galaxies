1195313d8a850edc227f3a22d09721135e3127ca
Lucas Brito <luzabri7@gmail.com>
2021-08-03 19:03:59 -0400

    U

--- src/get_cutouts.py
+++ src/get_cutouts.py
@@ -21,38 +21,61 @@ for _, i in images_df.iterrows():
     data, header = fits.getdata(i['path'], header=True)
     cgs[i['path']] = CutoutGenerator(data, header)
 
+# append test ra, dec coords to ensure script runs properly
 test_ra, test_dec = cgs[next(images_df.iterrows())[1]['path']].get_coords(50, 50)
 test_coords_df = pd.DataFrame([[test_ra, test_dec]], 
                             columns=['ra','dec'])
 all_red_galaxies_coord_df = all_red_galaxies_df.append(test_coords_df)
-print(all_red_galaxies_coord_df)
 
 cutouts = []
 
+# keep track of broken files TODO figure out why they are broken
 broken_files = set()
+found_coords = set()
+
+# total number of clusters inspected
 n_clusters = 0
 for __, cluster in unique_clusters_df.iterrows():
     n_clusters += 1
     print(str(n_clusters) + "/" + str(len(unique_clusters_df)))
+
+    # obtain CutoutGenerator object with which to test whether object 
+    # is in image.
     test_cg = cgs[cluster['path']]
+
+    # Skip current cluster if file broken
     if cluster['path'] in broken_files:
         continue
+
+    # Iterate over dataframe of coordinates
     for _, coord in all_red_galaxies_coord_df.iterrows():
         ra = coord['ra']
         dec = coord['dec']
-        if round(ra, 4) == round(test_ra, 4) and round(dec, 4) == round(test_dec, 4):
-            print("Currently trying test coordinates")
+
+        # if coordinate has already been found, skip
+        if (ra, dec) in found_coords:
+            continue
+
         try:
+            # If coordinates are not in the image, skip 
             if not test_cg.is_coord_in_image(ra, dec):
                 continue
+
+            found_coords.add((ra, dec))
             print('Coords ' + str(coord['ra']) + ', ' + str(coord['dec']) + \
                   ' found in cluster ' + str(cluster['path']))
+
+            # Obtain fits files corresponding to current cluster.
             file_df = images_df[images_df['cluster'] == cluster['cluster']]
+
+            # Obtain cutouts of this object for each file corresponding to this cluster.
             for ___, image in file_df.iterrows():
                 cg = cgs[image['path']]
                 data = cg.get_cutout(ra, dec)
                 cutouts.append(ra, dec, data)
+
         except Exception as e:
+            # Add file that excepts to list of broken files
             broken_files.add(cluster['path'])
             break
                              
