073811994214e3b21ece09d1f0eadf9289b76d56
Lucas Brito <luzabri7@gmail.com>
2021-08-02 20:35:07 -0400

    U

--- src/get_cutouts.py
+++ src/get_cutouts.py
@@ -16,23 +16,20 @@ all_red_galaxies_coord_df = all_red_galaxies_df[['ra', 'dec']]
 # get one row per unique 
 unique_clusters_df = images_df[images_df['band'] == 'z'].drop_duplicates(subset=['cluster'])
 
-all_found = 0
+cgs = {}
+for _, i in images_df.iterrows():
+    data, header = fits.getdata(i['path'], header=True)
+    cgs[i['path']] = CutoutGenerator(data, header)
+
 for __, cluster in unique_clusters_df.iterrows():
-    errors = 0
     for _, coord in all_red_galaxies_coord_df.iterrows():
-        path = cluster['path']
-        data, header = fits.getdata(path, header=True)
-        cg = CutoutGenerator(data, header)
+        cg = cgs[cluster['path']]
         try:
             if not cg.is_coord_in_image(coord['ra'], coord['dec']):
                 continue
             #print('Coords ' + str(coord['ra']) + ', ' + str(coord['dec']) + \
             #      ' found in cluster ' + str(cluster['path']))
         except Exception as e:
-            errors += 1
-            #print(coord['ra'], coord['dec'])
-            #print(cluster['path'])
-            #print('Error: ' + str(e))
-    print(errors)
+            continue 
 
 
