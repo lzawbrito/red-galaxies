7dac0cfe7af12268ab308a7547d06b58f8dea4c5
Robert Scheidegger <robert_scheidegger@brown.edu>
2021-10-10 15:18:55 -0400

    increase thread and memory limits

--- scripts/setup_training_data.sh
+++ scripts/setup_training_data.sh
@@ -1,2 +1,2 @@
-sbatch --mem=8G --output=outputs/setup_training_data.out -n 4 -t 06:00:00 scripts/run_script.sh "src/setup_training_data.py files/cluster_images.csv files/all_galaxies_rad.csv files/all_galaxies_rad.csv files/training_data/ -threads 3 -unknown_samples 1000"
+sbatch --mem=32G --output=outputs/setup_training_data.out -n 32 -t 06:00:00 scripts/run_script.sh "src/setup_training_data.py files/cluster_images.csv files/all_galaxies_rad.csv files/all_galaxies_rad.csv files/training_data/ -threads 30 -unknown_samples 1000"
 
