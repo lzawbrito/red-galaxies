7d23ef85cd5de118ad5a0bc765272f7f7a95dbb6
Robert Scheidegger <robert_scheidegger@brown.edu>
2021-10-10 14:39:59 -0400

    change run_script path

--- scripts/setup_training_data.sh
+++ scripts/setup_training_data.sh
@@ -1,2 +1,2 @@
-sbatch --mem=8G --output=outputs/setup_training_data.out -n 4 -t 06:00:00 run_script.sh src/setup_training_data.py files/cluster_images.csv files/all_galaxies.csv files/all_galaxies.csv files/training_data/ -threads 3 
+sbatch --mem=8G --output=outputs/setup_training_data.out -n 4 -t 06:00:00 scripts/run_script.sh src/setup_training_data.py files/cluster_images.csv files/all_galaxies.csv files/all_galaxies.csv files/training_data/ -threads 3 
 
