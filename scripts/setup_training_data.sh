6e911d97a7eb18ef5b099147888ae380e1b37f09
Robert Scheidegger <robert_scheidegger@brown.edu>
2021-11-05 23:57:14 -0400

    change directories of the data

--- scripts/setup_training_data.sh
+++ scripts/setup_training_data.sh
@@ -1,1 +1,1 @@
-sbatch --mem=32G --output=outputs/setup_training_data.out -n 32 -t 06:00:00 scripts/run_script.sh "src/setup_training_data.py files/cluster_images.csv files/all_galaxies_rad.csv files/all_galaxies_rad.csv files/training_data/ -threads 30 -unknown_samples 1000 -known_samples 500"
+sbatch --mem=32G --output=outputs/setup_training_data.out -n 32 -t 06:00:00 scripts/run_script.sh "src/setup_training_data.py files/cluster_images.csv files/all_huang_candidates_formatted.csv files/all_galaxies_rad.csv files/training_data/ -threads 30 -unknown_samples 1000 -known_samples 500"
